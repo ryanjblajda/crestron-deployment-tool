d44aed71fa7ae85c2bd48092f6f2b9af76221365
ryanjblajda <ryan.j.blajda@gmail.com>
2026-01-08 11:08:00 -0500

    feat: create a state machine for provisioning status

---
+++ Model/Deployment/Provisioning/ProvisioningStatus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrestronDeploymentTool.Model.Deployment.ProvisioningState
+{
+    internal enum ProvisioningStatus
+    {
+        WaitingUsername,
+        WaitingPassword,
+        WaitingVerification,
+        Complete,
+        Success,
+        Failure
+    }
+}
