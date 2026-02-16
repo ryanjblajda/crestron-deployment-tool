253d5693850bf4cab613e0b234a20ef1c8403577
ryanjblajda <ryan.j.blajda@gmail.com>
2026-02-16 09:32:17 -0500

    refactor: move so different areas can access passwords

---
+++ Model/Deployment/DeploymentResources.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrestronDeploymentTool.Model.Deployment
+{
+    internal static class DeploymentResources
+    {
+        internal static string? customUserName = null;
+        internal static string? customPassword = null;
+    }
+}
