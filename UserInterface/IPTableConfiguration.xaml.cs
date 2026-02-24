12ebd81da063acae4ff24ca04dc59af620ccb04f
ryanjblajda <ryan.j.blajda@gmail.com>
2026-02-24 08:47:36 -0500

    create: ip table configuration window

---
+++ UserInterface/IPTableConfiguration.xaml.cs
@@ -0,0 +1,32 @@
+﻿using CrestronDeploymentTool.Model.TargetDevices;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows;
+using System.Windows.Media;
+
+namespace CrestronDeploymentTool.UserInterface
+{
+    /// <summary>
+    /// Interaction logic for AddCrestronDevice.xaml
+    /// </summary>
+    public partial class IPTableConfiguration : Window
+    {
+        private List<IPTableEntry> Entries = new List<IPTableEntry>();
+
+        public IPTableConfiguration(List<IPTableEntry> ipTable)
+        { 
+            this.Owner = Application.Current.MainWindow;
+            this.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+            this.Title = "Add Known Crestron Device";
+            this.Entries = ipTable;
+            InitializeComponent();
+
+            if (DesignerProperties.GetIsInDesignMode(this)) { LoadElements(); }
+        }
+
+        private void LoadElements()
+        {
+            this.Entries.Add(new IPTableEntry("127.0.0.1", 30, "Gway", 41794));
+        }
+    }
+}
